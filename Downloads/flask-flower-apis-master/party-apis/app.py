fff47f3977272bccd5358508acfcf82b1b2f1874
Michelle Barbosae <michellebarbosa789@gmail.com>
2021-04-07 06:41:19 +0530

    Added Tables

--- Downloads/flask-flower-apis-master/party-apis/app.py
+++ Downloads/flask-flower-apis-master/party-apis/app.py
@@ -26,8 +26,8 @@ def obtainProject(token):
         return res.json()
 
 @app.route('/all-projects', methods=['GET'])
-def getProjects():
-   if request.method == 'GET':
+def allProjects():
+    if request.method == 'GET':
         main_dir = os.path.dirname(os.path.abspath(__file__))
         if not os.path.exists(os.path.join(main_dir, 'projects')):
             return returnResponse(409, "You have no projects")
@@ -43,10 +43,8 @@ def getProjects():
 
         return returnResponse(200, projects)
 
-@app.route('/delete-project/<id>', methods=['DELETE'])
+@app.route('/delete-project', methods=['DELETE'])
 def deleteProject():
-     if request.method == 'DELETE':
-       def deleteProject():
     if request.method == 'DELETE':
         main_dir = os.path.dirname(os.path.abspath(__file__))
         if not os.path.exists(os.path.join(main_dir, 'projects')):
@@ -63,7 +61,6 @@ def deleteProject():
         shutil.rmtree(os.path.join(project_path, projectName))
 
         return returnResponse(200, (projectName + " successfully deleted"))
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
